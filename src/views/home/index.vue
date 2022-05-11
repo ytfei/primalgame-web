13f14130a4614afbb3c4f479e048e424d3140be9
jay <keangjay@gmail.com>
2022-05-11 15:38:32 +0800

    feat(social): 添加 social 组件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { useNamespace } from 'src/hooks/useCommon'
+import SocialPlatform from 'comps/SocialPlatform.vue'
 const prefixCls = useNamespace('home')
 </script>
 
 <template>
-  <div :class="prefixCls.multiPrefixCls">Home</div>
+  <div :class="prefixCls.multiPrefixCls"><SocialPlatform></SocialPlatform></div>
 </template>
 
 <style lang="scss" scoped>
