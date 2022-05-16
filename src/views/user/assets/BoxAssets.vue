75e7c04158d8f87b0e9624a35341a93cf35aaf92
大博 <1095450045@qq.com>
2022-05-16 14:22:31 +0800

    feat(assets): 资产页面空状态添加

--- src/views/user/assets/BoxAssets.vue
+++ src/views/user/assets/BoxAssets.vue
@@ -6,7 +6,15 @@ const prefixCls = useNamespace('box-assets')
 
 <template>
   <div :class="prefixCls.multiPrefixCls">
-    <BoxCard></BoxCard>
+    <div class="box-card-content" v-if="1 === 2">
+      <BoxCard></BoxCard>
+      <BoxCard></BoxCard>
+      <BoxCard></BoxCard>
+      <BoxCard></BoxCard>
+    </div>
+    <div v-else class="box-nodata">
+      No blind box assets, can be purchased from <br/> the <span>campaign</span> page
+    </div>
   </div>
 </template>
 
@@ -16,5 +24,24 @@ $prefix-cls: '#{$namespace}-#{$moduleName}';
 $mobile-prefix-cls: '#{$namespace}-m-#{$moduleName}';
 
 .#{$prefix-cls} {
+  .box-card-content {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-between;
+    &::after {
+      content: '';
+      width: 370px;
+      height: 456px;
+    }
+  }
+  .box-nodata {
+    text-align: center;
+    font-size: 18px;
+    color: #FFFFFF;
+    margin-top: 150px;
+    span {
+      color: #FEC434;
+    }
+  }
 }
 </style>
